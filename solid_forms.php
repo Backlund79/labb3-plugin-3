a2971af4fcc693b5b8c77dc25948abb712fdff49
Emil Backlund <emilbacklund@outlook.com>
2020-11-26 09:50:39 +0100

    Form and form vars setup

--- solid_forms.php
+++ solid_forms.php
@@ -5,8 +5,24 @@
 */
 
 require 'acf-field-data.php';
+require 'inc/form.php';
 
+$obj_form = new Solid;
+
+
+add__shortcode('solid_form', array($obj_form, 'set_solid_form_func' ));
 class Solid
 {
-    //class code here
+    public $content = '';
+    public $name = $_POST['name'];
+    public $email = $_POST['email'];
+    public $message = $_POST['message'];
+    
+    public function set_solid_form_func($content){
+        $this-content = $content;
+    }
+
+    public function get_solid_form_func(){
+        return $this->content; 
+    }
 }
